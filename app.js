b763490d8b61363fc09be706ac8e9b5ca73198b4
Junaid7484 <junaid0808alam@gmail.com>
2026-08-05 22:00:17 +0530

    app.js mofify

--- app.js
+++ app.js
@@ -105,6 +105,6 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`server is working on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
 });
