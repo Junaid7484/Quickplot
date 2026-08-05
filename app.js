649360a4ced946fa951b373d462c97966b638fce
Junaid7484 <junaid0808alam@gmail.com>
2026-08-06 00:31:56 +0530

    Add missing layout file

--- app.js
+++ app.js
@@ -42,8 +42,9 @@ main()
 
 
 
-app.set("view engine", "ejs");
+
 app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
