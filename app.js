78c924183d3ee759e3c8f3db355eee7e401a85e0
Junaid7484 <junaid0808alam@gmail.com>
2026-08-06 00:04:59 +0530

    app.js mofify

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ main()
 
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
